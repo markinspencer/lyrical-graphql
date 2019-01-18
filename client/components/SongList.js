8c8d979c974fb8f395599210eaeff2ecd3dc4672
Spencer Markin <markinspencer@gmail.com>
2019-01-17 23:38:32 -0500

    finish implementing the song detail page/navigation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -18,7 +18,7 @@ class SongList extends Component {
 
     return songs.map(({ id, title }) => (
       <li key={id} className="collection-item">
-        {title}
+        <Link to={`/songs/${id}`}>{title}</Link>
         <i className="material-icons" onClick={() => this.onSongDelete(id)}>
           delete
         </i>
