d1f6e98b8b122d7cf3a3fd6ac8d7de6685d858ca
Spencer Markin <markinspencer@gmail.com>
2019-01-18 00:37:08 -0500

    render lyrics to screen

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
 class LyricList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderLyrics() {
+    return this.props.lyrics.map(({ id, content }) => (
+      <li key={id} className="collection-item">
+        {content}
+      </li>
+    ));
+  }
+
   render() {
-    return <ul> Lyric List </ul>;
+    return <ul className="collection">{this.renderLyrics()}</ul>;
   }
 }
 
