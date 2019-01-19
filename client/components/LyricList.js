0d37537963649e0bb5636c36dfa1d47ab1a638cc
Spencer Markin <markinspencer@gmail.com>
2019-01-18 22:01:18 -0500

    implement song like functionality

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { optimistic } from "apollo-client/optimistic-data/store";
 
 class LyricList extends Component {
   constructor(props) {
     super(props);
   }
 
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        likeLyric: {
+          id,
+          __typename: "LyricType",
+          likes: likes + 1
+        }
+      }
+    });
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => (
+    return this.props.lyrics.map(({ id, content, likes }) => (
       <li key={id} className="collection-item">
         {content}
+        <div className="vote-box">
+          {likes}
+          <i className="material-icons" onClick={() => this.onLike(id, likes)}>
+            thumb_up
+          </i>
+        </div>
       </li>
     ));
   }
@@ -18,4 +41,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
